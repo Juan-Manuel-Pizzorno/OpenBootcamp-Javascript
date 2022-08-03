5d437084773076f6649f302d9e8c6df832e07129
Juan-Manuel-Pizzorno <79718897+Juan-Manuel-Pizzorno@users.noreply.github.com>
2022-08-02 23:32:32 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-//This is the mensage of welcome.
-console.log("Hello bro, what are you doing?");
-
-/*This is all the comentary
-of the project, ok?*/
-
-//This is the exit.
-console.log("Goodbyee");
