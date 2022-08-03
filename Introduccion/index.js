55168c10aefdc646ec3cea70c16574c4805f5265
Juan-Manuel-Pizzorno <79718897+Juan-Manuel-Pizzorno@users.noreply.github.com>
2022-08-02 23:39:36 -0300

    Create index.js

---
+++ Introduccion/index.js
@@ -0,0 +1,8 @@
+//This is the mensage of welcome.
+console.log("Hello bro, what are you doing?");
+
+/*This is all the comentary
+of the project, ok?*/
+
+//This is the exit.
+console.log("Goodbyee");
